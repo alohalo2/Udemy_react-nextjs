00ecf073a91befa43b9919c6e03fc1d2d52429d7
DESKTOP-OEDPGKJ\wntjd <wntjd7892@naver.com>
2024-12-23 19:01:08 +0900

    24.12.23

--- TextBoard_react/frontend/src/components/Modal.jsx
+++ TextBoard_react/frontend/src/components/Modal.jsx
@@ -1,10 +1,20 @@
+import { useNavigate } from 'react-router-dom';
+
 import classes from './Modal.module.css';
 
-function Modal ({children, onClose}) {
+function Modal ({children}) {
+    const navigate = useNavigate();
+
+    function closeHandler() {
+        navigate('..'); // 이전 단계로 이동동
+    }
+
     return(
         <>
-            <div className={classes.backdrop} onClick={onClose}/>
-            <dialog open className={classes.modal}>{children}</dialog>
+            <div className={classes.backdrop} onClick={closeHandler}/>
+            <dialog open className={classes.modal}>
+                {children}
+            </dialog>
         </>
     );
 }
--- TextBoard_react/frontend/src/main.jsx
+++ TextBoard_react/frontend/src/main.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+import Posts, {loader as postsLoader} from "./routes/Posts";
+import NewPost, { action as newPostAction} from "./routes/NewPost";
+import PostDetails, {loader as postDetailsLoader} from "./routes/PostDetails";
+import RootLayout from "./routes/RootLayout";
+import "./index.css";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <RootLayout />,
+      children: [
+          { 
+            path: "/", 
+            element: <Posts />,
+            loader: postsLoader, // 6.4 이상 router에서 사용 가능
+            children: [
+              { path: "/create-post", element: <NewPost />, action: newPostAction },
+              { path: '/:postId', element:<PostDetails />, loader: postDetailsLoader }
+            ],
+        },
+      ],
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
