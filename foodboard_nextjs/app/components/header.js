1ce58085ba1366c0c293d2d3b4ebc32ae4aa98a2
DESKTOP-OEDPGKJ\wntjd <wntjd7892@naver.com>
2024-12-30 21:36:20 +0900

    24.12.30

---
+++ foodboard_nextjs/app/components/header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <>
+            <img src="/logo.png" alt="A server surrouned by magic sparkles."></img>
+            <h1>Welcome to this NextJS Course!</h1>
+        </>
+    );
+}
